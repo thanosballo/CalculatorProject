e0641ab840db01047d489c32e6bfb8ad3c413747
thanosbalo <thanosbalogiannis@gmail.com>
2023-01-15 15:30:58 +0200

    changes made

--- script.js
+++ script.js
@@ -13,7 +13,13 @@ function divide(a,b){
 function operate(a,b,code){
 
 }
+
+
 let string="";
+let sign="";
+let result="";
+let a="";
+let b="";
 const screen=document.querySelector(".screen");
 const buttons=document.querySelectorAll(".button");
 buttons.forEach(button=>{
@@ -22,5 +28,25 @@ buttons.forEach(button=>{
             string=`${string}${button.dataset.num}`;
             console.log(string);
         }
-    })
-})
+        else if (button.classList.contains("icons")){
+            sign=button.dataset.sign;
+            if (a===""){
+            a=parseInt(string);
+            string="";
+            }else {
+            b=parseInt(string);
+            string="";
+            console.log(b);
+            }
+            console.log(typeof(a),a,b,sign);
+        }else if (button.classList.contains("equal")){
+            switch (sign){
+                case "+":result=add(a,b);break;
+                case "-":result=substract(a,b);break;
+                case "*":result=multiply(a,b);break;
+                case "/":result=divide(a,b);break;
+            }
+            console.log(result,sign);
+        }
+    }
+        )})
